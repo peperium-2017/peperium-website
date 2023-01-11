edae8ac2332870f83301ccfbe9bd6350d4d24c35
crypt0biwan <crypt0biwan@gmail.com>
2023-01-11 11:20:21 +0100

    website update

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 let CurrentSeries = 1
 let html = `<div class="mb-3"><h2>Series 1</h2></div>
             <div class="row gx-4 gx-lg-5 row-cols-1 row-cols-md-3 row-cols-xl-4 justify-content-center">`
+let tableHtml = `<table class="table table-striped"><thead><tr>
+                    <th>Series</th>
+                    <th>Card</th>
+                    <th>Name</th>
+                    <th>Symbol</th>
+                    <th>CreationDate</th>
+                    <th>Supply</th>
+                    <th>Contract</th>
+                </tr></thead><tbody>`
 
 jsonData.forEach(asset => {
     const { Card, Contract, ['Creation Date']: CreationDate, Description, IPFS, ['IPFS hash']: IPFSHash, Image, Locked, Name, Series, Supply, Symbol, Type, Burned } = asset
 
     if(CurrentSeries !== Series) {
         html += `</div>
-        <div class="mb-3"><h2>Series ${Series}</h2></div>
+        <div class="mb-3"><h2>${Series === 'Fake Peperium' ? Series : `Series ${Series}`}</h2></div>
         <div class="row gx-4 gx-lg-5 row-cols-1 row-cols-md-3 row-cols-xl-4 justify-content-center">`
         
         CurrentSeries = Series
@@ -28,7 +37,7 @@ jsonData.forEach(asset => {
                 </div>
 
                 <div class="text-center">
-                    <h5 class="fw-bolder">S${Series}C${Card}<br />${Name}</h5>
+                    <h5 class="fw-bolder">${Series === 'Fake Peperium' ? Series : `S${Series}C${Card}`}<br />${Name}</h5>
                     supply: ${Supply}
                 </div>
             </div>
@@ -38,7 +47,7 @@ jsonData.forEach(asset => {
                         Show more info
                     </button>
 
-                    <a class="btn btn-outline-dark mt-auto" href="https://etherscan.io/token/${Contract}">View on etherscan</a>
+                    <!--<a class="btn btn-outline-dark mt-auto" href="https://etherscan.io/token/${Contract}">View on etherscan</a>-->
                 </div>
             </div>
         </div>
@@ -95,7 +104,7 @@ jsonData.forEach(asset => {
                                 </div>
                                 
                                 <div class="col-md-8">
-                                    ${IPFS ? '' : `Unfortunately this IPFS hash isn't online anymore, but this is the on-chain hash`}
+                                    ${IPFS ? '' : `<span class="text-warning"><i class="bi bi-exclamation-triangle-fill"></i></span> Unfortunately this IPFS hash isn't online anymore, but this is the on-chain hash<br />`}
                                     <i class="bi bi-caret-right-square-fill"></i> <a class="link-dark" href="https://ipfs.io/ipfs/${IPFSHash}" target="_blank">${IPFSHash}</a>
                                 </div>
                             </div>
@@ -108,6 +117,15 @@ jsonData.forEach(asset => {
                                     <i class="bi bi-caret-right-square-fill"></i> <a class="link-dark" href="https://etherscan.io/address/${Contract}" target="_blank">${Contract}</a>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    Locked
+                                </div>
+
+                                <div class="col-md-8">
+                                    ${Locked ? 'Yes, this supply is finite!!!' : `<span class="text-warning"><i class="bi bi-exclamation-triangle-fill"></i></span> No, this asset isn't locked, so it's possible the owner of thist contract can increase the supply<br />`}
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -118,11 +136,23 @@ jsonData.forEach(asset => {
             </div>
         </div>
     </div>`
+
+    tableHtml += `<tr>
+        <td style="white-space: nowrap;">${Series}</td>
+        <td>${Card}</td>
+        <td>${Name}</td>
+        <td>${Symbol.length > 7 ? `<span title="${Symbol}">${Symbol.substring(0, 7)}..</span>` : Symbol}</td>
+        <td style="white-space: nowrap;">${CreationDate.replace(/ /g, '&nbsp;')}</td>
+        <td>${Supply}</td>
+        <td><a target="_blank" href="https://etherscan.io/address/${Contract}">${Contract}</a></td>
+    </tr>`
 })
 
 html += '</div>'
+tableHtml += `</tbody></table>`
 
 document.querySelector('#cards .container').innerHTML = html
+document.querySelector('#technical #table-container').innerHTML = tableHtml
 
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
